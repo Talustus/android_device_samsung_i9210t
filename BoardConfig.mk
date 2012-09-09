18edc28628d35f6eff8e63e1d35866655781715d
Talustus <talustus@dream-irc.de>
2012-09-09 02:37:51 +0200

    Jellybean Import

---
+++ BoardConfig.mk
@@ -0,0 +1,89 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm8660
+-include device/samsung/msm8660-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/samsung/i9210t/BoardConfigVendor.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := skyrocket
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := SGHI727,SGH-I727,hercules,skyrocket,i9210t,celox,GTI9210,GTI9210T
+
+# Kernel
+TARGET_KERNEL_CONFIG        := cyanogenmod_i9210t_defconfig
+TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1
+BOARD_KERNEL_BASE           := 0x40400000
+BOARD_KERNEL_PAGESIZE       := 2048
+BOARD_FORCE_RAMDISK_ADDRESS := 0x41800000
+
+# cat /proc/emmc
+#dev:        size     erasesize name
+#mmcblk0p22: 00fffc00 00000200 "recovery"
+#mmcblk0p8: 01000000 00000200 "boot"
+#mmcblk0p24: 5ffffc00 00000200 "system"
+#mmcblk0p26: 13fffe00 00000200 "cache"
+#mmcblk0p25: 9ffffe00 00000200 "userdata"
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 694157312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2149580800
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# TWRP Recovery
+DEVICE_RESOLUTION := 480x800
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_NO_BATT_PERCENT := false
+TW_DEFAULT_EXTERNAL_STORAGE := false
+TW_FLASH_FROM_STORAGE := true
+TWRP_EVENT_LOGGING := false
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Suppress the WIPE command since it can brick our EMMC
+BOARD_SUPPRESS_EMMC_WIPE := true
+
+# Workaround for glitches while cropping bypass layers
+# TODO (orphaned) TARGET_NO_BYPASS_CROPPING := true
+
+# MTP
+# TODO (orphaned) BOARD_MTP_DEVICE := "/dev/mtp_usb"
+
+# Disable initlogo, Samsungs framebuffer is weird
+TARGET_NO_INITLOGO := true
+
+# Preload the boot animation to avoid jerkiness
+TARGET_BOOTANIMATION_PRELOAD := true
+
