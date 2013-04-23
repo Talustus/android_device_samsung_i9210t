8ebc82ec0fee437ef7d92e864e5fe435461ececa
Talustus <talustus@dream-irc.de>
2013-04-23 23:53:03 +0200

    Update Board Bootloader name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/samsung/i9210t/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := msm8660_surf
+TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := GT-I9210T,i9210t
