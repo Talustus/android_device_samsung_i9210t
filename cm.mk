98dd1b6ae138bac122a3fe9159fb59c3fa1832a7
Talustus <talustus@dream-irc.de>
2013-12-10 13:30:16 +0800

    I9210T: update build fingerprint to kitkat

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9210T TARGET_DEVICE=GT-I9210T BUILD_FINGERPRINT="samsung/GT-I9210T/GT-I9210T:4.1.2/IMM76D/UCLI3:user/release-keys" PRIVATE_BUILD_DESC="GT-I9210T-user 4.1.2 IMM76D UCLI3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9210T TARGET_DEVICE=GT-I9210T BUILD_FINGERPRINT="samsung/GT-I9210T/GT-I9210T:4.4/KRT16M/UCMC1:user/release-keys" PRIVATE_BUILD_DESC="GT-I9210T-user 4.4 KRT16M UCMC1 release-keys"
 
 PRODUCT_NAME := cm_i9210t
 PRODUCT_DEVICE := i9210t
